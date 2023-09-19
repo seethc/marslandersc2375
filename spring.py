22bb3df9cd6e8d9c5206ab50136436ff417cc742
seethc <sc2375@cam.ac.uk>
2023-09-19 23:11:29 +0100

    Assig 2 complete (mostly)

--- spring.py
+++ spring.py
@@ -9,6 +9,10 @@ k = 1
 x = 0
 v = 1
 
+# initialise start conditions for analytical check
+x_anal = x
+v_anal = v
+
 # simulation time, timestep and time
 t_max = 100
 dt = 0.1
@@ -17,6 +21,8 @@ t_array = np.arange(0, t_max, dt)
 # initialise empty lists to record trajectories
 x_list = []
 v_list = []
+x_anal_list = []
+v_anal_list = []
 
 # Euler integration
 for t in t_array:
@@ -31,9 +37,20 @@ for t in t_array:
     x = x + v_new * dt
     v = v_new
 
+    # analytical check
+    x_anal_list.append(x_anal)
+    v_anal_list.append(v_anal)
+
+    x_anal = np.sqrt(m/k) * np.sin(np.sqrt(k/m)*t)
+    v_anal = np.cos(np.sqrt(k/m)*t)
+
+
+
 # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
 x_array = np.array(x_list)
 v_array = np.array(v_list)
+x_anal_array = np.array(x_anal_list)
+v_anal_array = np.array(v_anal_list)
 
 # plot the position-time graph
 plt.figure(1)
@@ -42,6 +59,8 @@ plt.xlabel('time (s)')
 plt.grid()
 plt.plot(t_array, x_array, label='x (m)')
 plt.plot(t_array, v_array, label='v (m/s)')
+plt.plot(t_array, x_anal_array, label='x analy (m)')
+plt.plot(t_array, v_anal_array, label='v analy (m/s)')
 plt.legend()
 plt.show()
 
--- spring_verlet.py
+++ spring_verlet.py
@@ -9,9 +9,13 @@ k = 1
 x = 0 # value of x at time t = t + dt, taken from Euler integration
 v = 1
 
+# initialise start conditions for analytical check
+x_anal = x
+#v_anal = v
+
 # simulation time, timestep and time
-t_max = 100
-dt = 0.1
+t_max = 1000
+dt = 0.8
 t_array = np.arange(0, t_max, dt)
 
 x_prev = x - v*dt
@@ -19,6 +23,8 @@ x_prev = x - v*dt
 # initialise empty lists to record trajectories
 x_list = []
 v_list = []
+x_anal_list = []
+v_anal_list = []
 
 # Verlet integration
 for t in t_array:
@@ -34,9 +40,18 @@ for t in t_array:
     x_prev = x
     x = x_new
 
+    # analytical check
+    x_anal_list.append(x_anal)
+    #v_anal_list.append(v_anal)
+
+    x_anal = np.sqrt(m/k) * np.sin(np.sqrt(k/m)*t)
+    #v_anal = np.cos(np.sqrt(k/m)*t)
+
 # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
 x_array = np.array(x_list)
 v_array = np.array(v_list)
+x_anal_array = np.array(x_anal_list)
+v_anal_array = np.array(v_anal_list)
 
 # plot the position-time graph
 plt.figure(1)
@@ -44,6 +59,9 @@ plt.clf()
 plt.xlabel('time (s)')
 plt.grid()
 plt.plot(t_array, x_array, label='x (m)')
-plt.plot(t_array, v_array, label='v (m/s)')
+#plt.plot(t_array, v_array, label='v (m/s)')
+plt.plot(t_array, x_anal_array, label='x analy (m)')
+#plt.plot(t_array, v_anal_array, label='v analy (m/s)')
 plt.legend()
 plt.show()
+
